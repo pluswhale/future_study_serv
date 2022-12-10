c80a9f6bfd60bdbba8b969c818001975271bd817
Egor Dultsev <listenmy@yandex.ru>
2022-12-10 14:18:05 +0300

    fix cors

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(passport.session());
 
 app.use(cors({
     origin: "http://localhost:3000/",
-    credentials: 'include',
+    credentials: true,
     allowedHeaders: {
         "Access-Control-Allow-Headers": ['Content-Type','Authorization']
     }
